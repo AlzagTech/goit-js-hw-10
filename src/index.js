e9a8cb27f454336a41bb1741c28a2f79495c7d47
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-24 13:58:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-infot'),
+};
+
+refs.input.addEventListener('input', onInput);
+
+function onInput(event) {
+  const name = event.target.value;
+
+  fetchCountries(name).then(countries =>
+    refs.list.insertAdjacentHTML('beforeend', renderCountriesList(countries))
+  );
+}
+
+function renderCountriesList(arr) {
+  return arr
+    .map(
+      item =>
+        `<li>
+        <svg xmlns=${item.flags.svg}></svg>
+        <h2>${item.name.official}</h2>
+      </li>`
+    )
+    .join('');
+}
