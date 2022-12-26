ad3dc8c5c09cc3b92a3dfb030fe64b75c089fbdf
Oleksandr Zahorskyi <alex.alzag.zagorsky@gmail.com>
2022-12-26 20:14:47 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,10 +3,12 @@ export function fetchCountries(name) {
 
   return fetch(url)
     .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
       return response.json();
     })
     .then(data => {
-      console.log(data);
       return data;
     });
 }
